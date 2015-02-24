c6521e3ff552cb11e9f82321f5c82d9e8736f17f
kaiomoreira <kaio@roadapps.net>
2015-02-24 15:36:05 -0300

    folders

---
+++ build/coffee/home.js
@@ -0,0 +1,10 @@
+var countdown, num;
+
+countdown = (function() {
+  var i, results;
+  results = [];
+  for (num = i = 10; i >= 1; num = --i) {
+    results.push(num);
+  }
+  return results;
+})();
